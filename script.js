204990bc3337669c212d0874634166e16cd2729f
an133444 <adsantana@ucundinamarca.edu.co>
2025-09-18 09:00:56 -0500

    codigo funcional

--- script.js
+++ script.js
@@ -1,17 +1,9 @@
 function encriptar(){
-  let vc = 0;
-  let mensaje = document.getElementById("mse").value;
-  let respuesta = "";
-  let salida = ""
-  let caracter = "";
-  let numero = 0;
-  for (vc = 0; vc < mensaje.length; vc++) {
-      
-    respuesta = respuesta + "," + mensaje[vc].charCodeAt(0);
-    numero = mensaje[vc].charCodeAt(0) + 10;
-    caracter = String.fromCharCode(numero);
-    salida = salida + caracter;
-  }
-
-  document.getElementById("se").innerHTML = salida;
+    let vc = 0;
+    let mensaje = document.getElementById("mse").value;
+    respuesta = "";
+    for(vc = 0; vc < mensaje.length; vc = vc+1){
+        respuesta = respuesta + "," + mensaje[vc].charCodeAt(0);
+    }
+    document.getElementById("se").innerHTML = respuesta;
 }
